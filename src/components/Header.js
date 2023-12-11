b41b7c3f3250b9dff65946851f29f0b6577cc8aa
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-12 05:40:09 +0600

    Header Component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,16 +11,16 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header
-      className="bg-pink-200 fixed w-full px-[30px]
+      className="fixed w-full px-[30px]
    lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center"
     >
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         {/* logo  */}
         <Link to={"/"}>
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="" className="max-w-[200px]" />
         </Link>
         {/* nav -initially hidden -show on desktop mode */}
-        <nav className="hidden lg:flex gap-x-12 font-semibold">
+        <nav className="hidden xl:flex gap-x-12 font-semibold">
           <Link
             to={"/"}
             className="text-[#696c6d] hover:text-primary transition"
@@ -49,12 +49,12 @@ const Header = () => {
             Contact
           </Link>
         </nav>
-
-        {/* socials */}
-        <Socials />
-        {/* mobile nav */}
-        <MobileNav />
       </div>
+
+      {/* socials */}
+      <Socials />
+      {/* mobile nav */}
+      <MobileNav />
     </header>
   );
 };
