baf833bc368208bb47e0a2583d6858b8ebc8fdad
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-13 23:08:43 +0600

    Animations added to all the pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,22 @@ import WomanImg from "../img/about/woman.png";
 //import link
 import { Link } from "react-router-dom";
 
+//import motion
+import { motion } from "framer-motion";
+
+//import transition
+import { transition1 } from "../transitions";
+
 const About = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
-      
         {/* text & img wrapper */}
         <div
           className="flex flex-col lg:flex-row h-full
@@ -22,13 +33,17 @@ const About = () => {
           </div>
 
           {/* text */}
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={transition1}
             className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col
            justify-center items-centerlg:items-start"
           >
             <h1 className="h1">About me</h1>
             <p className="mb-12 max-w-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus
               <b> scelerisque viverra</b> at id aenean scelerisque. Nec eget
               condimentum eitam leo
               <br />
@@ -36,11 +51,13 @@ const About = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            <Link to={"/portfolio"} className="btn">View my work</Link>
-          </div>
+            <Link to={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,81 @@
 import React from "react";
 
+//import images
+import WomanImg from "../img/contact/woman.png";
+
+//import motion
+import { motion } from "framer-motion";
+
+//import transition
+import { transition1 } from "../transitions";
+
 const Contact = () => {
-  return <section className="section  bg-red-200">Contact</section>;
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section bg-white"
+    >
+      <div className="container mx-auto h-full">
+        <div
+          className="flex flex-col lg:flex-row h-full items-center
+        justify-start pt-36 gap-x-8 text-center lg:text-left"
+        >
+          {/* background */}
+          <motion.div
+            className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72
+          -z-10"
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+          ></motion.div>
+          {/* text & form */}
+          <div className="lg:flex-1 lg:pt-32 px-4">
+            <h1 className="h1">Contact me</h1>
+            <p className="mb-12">I would love to get suggestions from you.</p>
+            <form className="flex flex-col gap-y-4">
+              <div className="flex gap-x-10">
+                <input
+                  className="outline-none border-b border-b-primary h-[60px] bg-transparent
+                font-secondary w-full pl-3 placeholder:text-[#75789]"
+                  type="text"
+                  placeholder="Your name"
+                />
+                <input
+                  className="outline-none border-b border-b-primary h-[60px] bg-transparent
+                font-secondary w-full pl-3 placeholder:text-[#75789]"
+                  type="text"
+                  placeholder="Your email"
+                />
+              </div>
+              <input
+                className="outline-none border-b border-b-primary h-[60px] bg-transparent
+                font-secondary w-full pl-3 placeholder:text-[#75789]"
+                type="text"
+                placeholder="Your message"
+              />
+              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">
+                Send it
+              </button>
+            </form>
+          </div>
+          {/* image */}
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{ transition: transition1, duration: 1.5 }}
+            className="lg:flex-1"
+          >
+            <img src={WomanImg} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
