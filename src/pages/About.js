cf3129bd5de5cf14f3980921a4faade14886d203
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-13 23:20:49 +0600

    Updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -39,7 +39,7 @@ const About = () => {
             exit={{ opacity: 0, y: "-80%" }}
             transition={transition1}
             className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col
-           justify-center items-centerlg:items-start"
+           justify-center items-center lg:items-start"
           >
             <h1 className="h1">About me</h1>
             <p className="mb-12 max-w-sm">
